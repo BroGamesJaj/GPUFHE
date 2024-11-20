5b8caf097097b924ca2649666ed1e1853562cf8d
BroGames <hunempire11@gmail.com>
2024-11-20 12:23:09 +0100

    change 0.8

--- src/bgvfhe_gpu.cu
+++ src/bgvfhe_gpu.cu
@@ -5,12 +5,12 @@ __global__ void add(long long int* a,long long int* b,long long int* c){
     c[i] = a[i] + b[i];
 }
 
-__managed__ long long int vector_a[1073741824], vector_b[1073741824], vector_c[1073741824];
+__managed__ long long int vector_a[65536], vector_b[65536], vector_c[65536];
 
 int main(){
-    for (int i = 0; i < 1073741824; i++){
+    for (int i = 0; i < 65536; i++){
         vector_a[i] = i;
-        vector_b[i] = 1073741824 - i;
+        vector_b[i] = 65536 - i;
     }
 
     add<<<1,256>>>(vector_a, vector_b, vector_c);
@@ -19,7 +19,7 @@ int main(){
 
     
     long long int res = 0;
-    for (int i = 0; i < 1073741824; i++){
+    for (int i = 0; i < 65536; i++){
         res += vector_c[i];
     }
     printf("sum result: % d",res);
