dbd23c78ceb9b24085e07a75cb573455bb39dbc7
BroGames <hunempire11@gmail.com>
2024-11-20 11:27:05 +0100

    change 0.1

--- src/bgvfhe_gpu.cu
+++ src/bgvfhe_gpu.cu
@@ -10,15 +10,17 @@ __managed__ int vector_a[256], vector_b[256], vector_c[256];
 int main(){
     for (int i = 0; i < 256; i++){
         vector_a[i] = i;
-        vector_b[i] = 256 -i;
+        vector_b[i] = 256 - i;
     }
 
     add<<<1,256>>>(vector_a, vector_b, vector_c);
 
     cudaDeviceSynchronize();
 
+    
     int res = 0;
     for (int i = 0; i < 256; i++){
+        printf("vector_c %d %d",i,vector_c[i]);
         res += vector_c[i];
     }
     printf("sum result: % d",res);
