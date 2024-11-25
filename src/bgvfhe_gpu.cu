de286e78d45973d88c2d58e3b4160b69277c6b9e
BroGames <hunempire11@gmail.com>
2024-11-25 09:14:36 +0100

    revert

--- src/bgvfhe_gpu.cu
+++ src/bgvfhe_gpu.cu
@@ -1,75 +1,39 @@
 #include <stdio.h>
 #include "sub/poly.h"
 #include "sub/poly_eqs.h"
+#include <random>
 #include <inttypes.h>
-#include <cuda_runtime.h>
-#include <stdlib.h>
 
-#define N 10000000
-
-__global__ void polyMult_gpu(int* a, int* b, int* c, int size){
-    int i = blockIdx.y * blockIdx.x + threadIdx.x;
-    if(i < size){
-        c[i] = a[i] * b[i];
-    }
+__global__ void add(int* a, int* b, int* c){
+    int i = threadIdx.x + blockIdx.y * blockDim.x;
+    c[i] = a[i] + b[i];
 }
 
-void PolyMult_cpu(int* p1, int* p2, int* p3, int size){
-
-        for (int i=0; i<size; i++) { 
-            for (int j=0; j<size; j++){
-                p3[i+j] = p1[i]*p2[j]; 
-            }
-        } 
-    }
-void init_array(int *array, int n) {
-    for (int i = 0; i < n; i++) {
-        array[i] = (int)rand() / RAND_MAX;
-    }
-}
+__managed__ int vector_a[256], vector_b[256], vector_c[256];
 
 int main(){
-    int *h_array1, *h_array2, *h_array_cpu; // *h_array_gpu;
-    // int *d_array1, *d_array2, *d_array_gpu;
-    size_t size = N * sizeof(int);
-    h_array1 = (int*)malloc(size);
-    h_array2 = (int*)malloc(size);
-    // h_array_gpu = (int*)malloc(size);
-    h_array_cpu = (int*)malloc(size);
-    init_array(h_array1,size);
-    init_array(h_array2,size);
-    /*
-    cudaMalloc(&d_array1,size);
-    cudaMalloc(&d_array2,size);
-    cudaMalloc(&d_array_gpu,size);
 
-    cudaMemcpy(d_array1, h_array1, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_array2, h_array2, size, cudaMemcpyHostToDevice);
+    Polinomial array(10);
+    Polinomial array2(10);
+    Polinomial array3(19);
+    std::random_device rd;                     // Seed for randomness
+    std::mt19937 gen(rd());                    // Mersenne Twister generator
+    std::uniform_int_distribution<size_t> dis(1, 10); // Uniform distribution [1, 10]
 
-    int block_num = (N + 256 -1) / 256;
-    polyMult_gpu<<<block_num,256>>>(d_array1, d_array2, d_array_gpu, size);
-    cudaDeviceSynchronize();
-
-    cudaMemcpy(h_array_gpu, d_array_gpu, size, cudaMemcpyDeviceToHost);
-    
-    for (int i=0; i<size; i++) 
+    // Fill the array with random numbers
+    for (size_t i = 0; i < array.getSize(); ++i) {
+        array[i] = dis(gen); // Generate random number and assign to array
+    }
+    for (size_t i = 0; i < array2.getSize(); ++i) {
+        array2[i] = dis(gen); // Generate random number and assign to array
+    }
+    array3 = poly_eqs::PolyMult(array,array2);
+    for (int i=0; i<array3.getSize(); i++) 
     { 
-       printf( "%d" PRIu64, *h_array_gpu); 
+       printf( "%" PRIu64, array3[i]); 
        if (i != 0) 
         printf("x^%d",i) ; 
-       if (i != size-1) 
-       printf(" + "); 
-    } 
-    printf("\n");
-    */
-    printf("hello");
-    PolyMult_cpu(h_array1, h_array2, h_array_cpu, size);
-    for (int i=0; i<size; i++) 
-    { 
-       printf( "%d" PRIu64, *h_array_cpu); 
-       if (i != 0) 
-        printf("x^%d",i) ; 
-       if (i != size-1) 
+       if (i != array3.getSize()-1) 
        printf(" + "); 
     } 
 }
